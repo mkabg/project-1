af49941c538ce91d5d3752a566957f50ff60b187
mkabg <m7110645k@gmail.com>
2025-05-12 16:40:55 +0300

    add signature

--- project 1/Program.cs
+++ project 1/Program.cs
@@ -8,6 +8,31 @@ namespace project_1
 {
     internal class Program
     {
+        static void print(string message);
+
+        static string getSeries(string input);
+
+        static bool validate(string input);
+        static void showMenue();
+
+        static List <int> convertToNumbers(string input);
+
+        static List <int> reverseTheSeries(int input);
+
+        static List <int> getSortedList(int input);
+
+        static int getMax(int input);
+
+        static int getMin(int input);
+
+        static int getAverage(int input);
+
+        static int getNumberOfElements(int input);
+
+        static int getSum(int input);
+
+
+
         static void Main(string[] args)
         {
         }
